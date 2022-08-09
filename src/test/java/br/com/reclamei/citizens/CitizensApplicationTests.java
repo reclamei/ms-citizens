cbd4c72525a7f4a8929727c4e289e4f2f8dfbe60
gustavo.zucolotto <gustavo.zucolotto@picpay.com>
2022-08-09 19:44:51 -0300

    starting project

---
+++ src/test/java/br/com/reclamei/citizens/CitizensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reclamei.citizens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
