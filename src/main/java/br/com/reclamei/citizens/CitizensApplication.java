c017819b16e9d8c39c54f8c39f897b11cf91227b
livia <liviaaurich2@gmail.com>
2023-10-26 23:06:08 -0300

    Incluindo discovery client

--- src/main/java/br/com/reclamei/citizens/CitizensApplication.java
+++ src/main/java/br/com/reclamei/citizens/CitizensApplication.java
@@ -2,11 +2,8 @@ package br.com.reclamei.citizens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-
-@EnableDiscoveryClient
 @EnableMongoRepositories
 @SpringBootApplication
 public class CitizensApplication {
